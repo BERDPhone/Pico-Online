fe9f777da619917e11a2b92d63412b5cbec2e929
BjornTheProgrammer <bjornbeishline@icloud.com>
2022-07-24 21:27:06 -0700

    added GET for pull.js

--- api/routes/pull.js
+++ api/routes/pull.js
@@ -4,6 +4,13 @@ var router = express.Router();
 /* 
 GET norsemarketing.com/api/message => {"message": "Hello World!"}
 */
+router.get('/', (req, res, next) => {
+	console.log(req);
+	res.json({
+		"message": "GET Request recieved!",
+	});
+});
+
 router.post('/', function(req, res, next) {
 	console.log("body: ", req.body);
 	res.json({
