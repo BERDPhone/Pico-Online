c03f120fc08c45345535b2c4ff0754a0ff8c0a50
BjornTheProgrammer <bjornbeishline@icloud.com>
2022-07-25 01:28:37 -0700

    testing webhook

--- client/src/App.js
+++ client/src/App.js
@@ -9,6 +9,9 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+	<p>
+		Testing automatic updates via github webhook
+	</p>
         <a
           className="App-link"
           href="https://reactjs.org"
