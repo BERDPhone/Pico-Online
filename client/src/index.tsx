80b1233528a64252df7dcaca25886ff698ed1100
BjornTheProgrammer <75190918+BjornTheProgrammer@users.noreply.github.com>
2022-07-26 14:54:35 -0700

    basic dash and yarn integration done

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -3,11 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { TerminalContextProvider } from "react-terminal";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(
+	document.getElementById('root') as HTMLElement
+);
 root.render(
 	<React.StrictMode>
-		<App />
+		<TerminalContextProvider>
+			<App />
+		</TerminalContextProvider>
 	</React.StrictMode>
 );
 
